97eb2de3c8ee044e4728f272c7ebea994afc4438
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-04-13 14:25:39 +0200

    Add views and 404 page

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.show = (name) => {
+    res.sendFile(path.join(__dirname, `/views/${name}`));
+  };
+  next();
+});
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req, res) => res.show('index.html'));
+
+app.get('/home', (req, res) => res.show('index.html'));
+
+app.get('/about', (req, res) => res.show('index.html'));
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'public/404.jpg'));
+});
+app.listen(8000, () => {
+  console.log('Sever is runnig on port 8000');
+});
